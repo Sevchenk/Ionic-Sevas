dc6f6b4f03d0d7b9abfb165eaa6836fb24c7f743
Sevchenk <sebastianroj.14@gmail.com>
2024-08-29 14:20:39 -0400

    Inicio

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,14 +1,26 @@
 import './ExploreContainer.css';
+import React from 'react';
+import { IonCard, 
+          IonCardContent, 
+          IonCardHeader, 
+          IonCardSubtitle, 
+          IonCardTitle } from '@ionic/react';
 
-interface ContainerProps { }
 
-const ExploreContainer: React.FC<ContainerProps> = () => {
+
+function ExploreContainer() {
   return (
-    <div id="container">
-      <strong>Ready to create an app?</strong>
-      <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-    </div>
-  );
-};
+    <IonCard>
+      <img alt="calaca" src="calaca.png" />
+      <IonCardHeader>
+        <IonCardTitle>
+          <b>GOL DE ORO</b>
+        </IonCardTitle>
+        <IonCardSubtitle>GOL DE ORO </IonCardSubtitle>
+      </IonCardHeader>
 
+      <IonCardContent>Here's a small text description for the card content. Nothing more, nothing less.</IonCardContent>
+    </IonCard>
+  );
+}
 export default ExploreContainer;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,7 +7,7 @@ const Home: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Tu mama</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
